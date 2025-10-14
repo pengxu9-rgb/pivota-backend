ea2e062c7d52620ff857870053169929658ddbcc
Peng@chydan <pengchydan@Pengchydans-MacBook-Pro.local>
2025-10-14 08:27:31 +0800

    Add debug routes to test WebSocket event publishing

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from routes.test_data_routes import router as test_data_router
 from routes.simple_ws_routes import router as simple_ws_router
 from routes.auth_routes import router as auth_router
 from routes.auth_ws_routes import router as auth_ws_router
+from routes.debug_routes import router as debug_router
 
 # Service routers (only include what exists)
 try:
@@ -76,6 +77,7 @@ app.include_router(merchant_router)
 app.include_router(payment_router)
 app.include_router(auth_router)  # Authentication
 app.include_router(auth_ws_router)  # Authenticated WebSocket
+app.include_router(debug_router)  # Debug routes
 app.include_router(dashboard_router)  # Dashboard API
 app.include_router(dashboard_api_router)  # New Dashboard API
 app.include_router(payment_routes_router)  # Payment Processing API
