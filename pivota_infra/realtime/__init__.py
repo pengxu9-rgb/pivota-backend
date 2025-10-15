b17e21a2a6876fff9af8f5f79cb0de26583f3dd8
Peng <peng@chydan.com>
2025-10-15 17:00:10 +0800

    Add missing realtime exceptions (AuthorizationError, etc)

--- pivota_infra/realtime/__init__.py
+++ pivota_infra/realtime/__init__.py
@@ -1,1 +1,22 @@
 # Real-time metrics and WebSocket infrastructure
+
+# Define custom exceptions for realtime module
+class AuthorizationError(Exception):
+    """Raised when authorization fails"""
+    pass
+
+class NotConnectedError(Exception):
+    """Raised when not connected to realtime service"""
+    pass
+
+class RealtimeError(Exception):
+    """Base exception for realtime errors"""
+    pass
+
+class ConnectionError(RealtimeError):
+    """Raised when connection fails"""
+    pass
+
+class SubscriptionError(RealtimeError):
+    """Raised when subscription fails"""
+    pass
